6150be98576a612e4a1e55f89282e502bf53b491
Sourabh Khandelwal <sorbkhandelwal@gmail.com>
2018-05-29 18:36:08 +0530

    Update Reversal Algorithm to rotate an array by k elements in O(N) time an in-place.cpp

--- Reversal Algorithm to rotate an array by k elements in O(N) time an in-place.cpp
+++ Reversal Algorithm to rotate an array by k elements in O(N) time an in-place.cpp
@@ -9,7 +9,20 @@ void solve() {
     for(auto &i : ar)
         cin >> i;
     cin >> k;
-
+    
+    // take mod to handle the case when k >= n, mod is written in this way to handle the cases when k < 0
+    k = (k % n + n) % n
+        
+    /* 
+        This algorithm is for rotating array to the left, for rotating array to the right, do replace lines 26-28 with the
+        below code:
+       
+        reverse(ar + n - k, ar + n);
+        reverse(ar, ar + n - k);
+        reverse(ar, ar + n);
+    
+    */
+        
     reverse(ar, ar + k);
     reverse(ar + k, ar + n);
     reverse(ar, ar + n);
